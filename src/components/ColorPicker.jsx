f1d4aadc980486796cda0bf6ab1c2619091f6a11
Osekobo <collinsboseko2005@gmail.com>
2025-11-26 19:39:08 +0300

    Color picker not done yet

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -8,6 +8,14 @@ function Color() {
       <h1>{count}</h1>
       <button onClick={() => { setCount(count + 1) }}>Increase</button>
       <button onClick={() => { setCount(count - 1) }}>Decrease</button>
+      <select class="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">Red</option>
+        <option value="2">Blue</option>
+        <option value="3">Green</option>
+        <option value="3">Yellow</option>
+      </select>
+      <h1>Your favourite color is:</h1>
     </>
   )
 };
