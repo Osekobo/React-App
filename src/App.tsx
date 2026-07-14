1bc0d83e4f674c3aaed6f35d4f8962a236df99cf
Oseko <collinsoseko2003@gmail.com>
2026-07-14 20:00:19 +0300

    Second component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
 import Header from "./components/Header";
+import Itemcard from "./components/Itemcard";
+
 export default function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Itemcard />
+    </div>
+  );
 }
---
+++ src/components/Itemcard.tsx
@@ -0,0 +1,7 @@
+export default function Itemcard() {
+  return (
+    <div>
+      <h1>This is the item card</h1>
+    </div>
+  );
+}
