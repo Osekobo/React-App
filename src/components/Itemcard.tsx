0338369fa5f28d595d98a6408f4d0bec944b5244
Oseko <collinsoseko2003@gmail.com>
2026-07-20 21:15:05 +0300

    Props

--- src/components/Itemcard.tsx
+++ src/components/Itemcard.tsx
@@ -1,7 +1,15 @@
-export default function Itemcard() {
+interface itemCardprops {
+  name:string;
+  price:number;
+  inStock:boolean;
+}
+
+export default function Itemcard({ name, price, inStock }: itemCardprops) {
   return (
     <div>
       <h1>This is the item card</h1>
+      <div style={{}}></div>
+      <h2>{name}</h2>
     </div>
   );
 }
