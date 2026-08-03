779148c20cfbb89ecf404b8347a0501e403e3243
Oseko <collinsoseko2003@gmail.com>
2026-08-03 23:40:09 +0300

    useState2

--- src/components/Itemcard.jsx
+++ src/components/Itemcard.jsx
@@ -2,20 +2,19 @@ import { useState } from "react";
 
 export default function Itemcard() {
   const [count, setCount] = useState(0);
-  function handleAdd() {
-    setCount(count + 1);
-  }
-  function handleMinus() {
-    setCount(count - 1);
-  }
+  // function handleAdd() {
+  //   setCount(count + 1);
+  // }
+  // function handleMinus() {
+  //   setCount(count - 1);
+  // }
   return (
     <div>
       <h1>Count:{count}</h1>
-      <button onClick={handleAdd}>Click Me +</button>
+      <button onClick={() => setCount(count + 1)}>Click Me +</button>
       <br />
       <br />
-      <button onClick={handleMinus}>Click Me -</button>
+      <button onClick={() => setCount(count - 1)}>Click Me -</button>
     </div>
   );
 }
-
