118aa335f26f395a1eeacba413d8a52aaacae389
Oseko <collinsoseko2003@gmail.com>
2026-08-05 10:44:42 +0300

    Forms with handleChange

--- src/components/ItemForm.jsx
+++ src/components/ItemForm.jsx
@@ -2,33 +2,38 @@ import { useState } from "react";
 
 export default function ItemForm() {
   const [name, setName] = useState("");
-  const [num, setNum] = useState("");
+  const [price, setPrice] = useState("");
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(name);
+    console.log(price);
+  }
   return (
     <div>
       <h1>Item Form</h1>
-      <form>
-        <label>Name </label>
+      <form onSubmit={handleSubmit}>
+        <label>Name: </label>
         <input
           type="text"
-          placeholder="Enter Name"
+          placeholder="Enter name"
           value={name}
           onChange={(event) => setName(event.target.value)}
         ></input>
         <br />
         <br />
-        <label>Price </label>
+        <label>Price: </label>
         <input
           type="number"
-          placeholder="Price"
-          value={num}
-          onChange={(event)=>setNum(event.target.value)}
+          placeholder="Enter price"
+          value={price}
+          onChange={(event) => setPrice(event.target.value)}
         ></input>
         <br />
         <br />
         <button>Submit</button>
       </form>
-      <h1>Your name is {name}</h1>
-      <h1>Your number is {num}</h1>
+      <h1>You have entered {name}</h1>
+      <h1>The price is {price}</h1>
     </div>
   );
 }
